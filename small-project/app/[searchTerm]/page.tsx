3899d813f317084d4cdfbf2ac1c46ae1090f97f7
paingpyaethu <paingpyaethu@gmail.com>
2023-04-03 15:21:33 +0630

    displaying result

--- small-project/app/[searchTerm]/page.tsx
+++ small-project/app/[searchTerm]/page.tsx
@@ -1,11 +1,22 @@
+import getWikiResults from "@/lib/getWikiResults"
+
 type Props = {
     params: {
         searchTerm: string
     }
 }
 
-export default function page({ params: {searchTerm}}: Props) {
-  return (
-    <div>page</div>
-  )
+export default async function page({ params: {searchTerm}}: Props) {
+    const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+    const data = await wikiData
+    const results: Result[] | undefined = data?.query?.pages
+
+    const content = (
+        <main>
+            {results ? Object.values(results).map(result => {
+                return <p>{JSON.stringify(result)}</p>
+            }) : <h2 className="p-2 text-xl">{`${searchTerm} Not found!`}</h2>}
+        </main>
+    )
+  return content
 }
